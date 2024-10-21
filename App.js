085c1671c5da0c701b4fadee5696e08742285f88
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-10-21 02:00:55 -0300

    feat: :sparkles: Adiciona estrutura inicial
    Este commit adiciona a estrutura inicial do projeto.

---
+++ App.js
@@ -0,0 +1,32 @@
+// App.js
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import GetStarted from "./src/screens/GetStarted";
+import Home from "./src/screens/Home";
+import Scan from "./src/screens/Scan";
+import AddDocument from "./src/screens/AddDocument";
+import DocumentDetails from "./src/screens/DocumentDetails";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="GetStarted"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="GetStarted" component={GetStarted} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Scan" component={Scan} />
+        <Stack.Screen name="AddDocument" component={AddDocument} />
+        <Stack.Screen name="DocumentDetails" component={DocumentDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
